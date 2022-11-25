44a40fae6b4ac9f2b36042b3c4e57f982393a83c
omar saadeh <omarsaadeh11@gmail.com>
2022-11-25 06:33:52 -0800

    Updated a Published Package

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,33 +1,56 @@
+const Joi = require("joi");
 const express = require("express");
-//create our express application
 const app = express();
 
-//callback: this is the function that will be called
-//when we have an HTTP GET req to this endpoint.
-//callback Fn also called Route Handler
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
-});
+app.use(express.json());
 
-app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
-});
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
-// /api/courses/1
-
-// app.get("/api/courses/:id", (req, res) => {
-//   res.send(req.params.id);
+// app.get("/", (req, res) => {
+// res.send("Hello World!!!");
 // });
 
-app.get("/api/posts/:year/:month", (req, res) => {
-  // res.send(req.params);
-  // res.send(req.params.year);
-  //send to client
-  res.send(req.query);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
+
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+  // console.log(result);
+
+  if (result.error) {
+    //400 Bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+// app.get("/api/courses/:id", (req, res) => {
+// cz req.params.id return string
+// const course = courses.find((c) => c.id === parseInt(req.params.id));
+// if (!course)
+// res.status(404).send("The Course with the given ID was not found.");
+// res.send(course);
+// });
 
 //PORT
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
