bb69994faf2e7edb17aeaca538e61aca72cd3830
omar saadeh <omarsaadeh11@gmail.com>
2022-11-28 12:54:50 -0800

    Building a Restful API using express

--- express-demo/app.js
+++ express-demo/app.js
@@ -12,78 +12,71 @@
 //get all genre
 // create a new genre
 // update or delete
-
 const Joi = require("joi");
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 
-let genres = [
-  { id: 1, genre: "Action" },
-  { id: 2, genre: "Horror" },
-  { id: 3, genre: "Drama" },
-  { id: 4, genre: "Comdey" },
-  { id: 5, genre: "Romance" },
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" },
 ];
-//read
+
 app.get("/api/genres", (req, res) => {
   res.send(genres);
 });
-//create
+
 app.post("/api/genres", (req, res) => {
-  const schema = {
-    genre: Joi.string().min(3).required(),
-  };
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-  const result = Joi.validate(req.body, schema);
-
-  if (result.error) {
-    //400 Bad request
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
-
-  let course = {
+  const genre = {
     id: genres.length + 1,
-    genre: req.body.genre,
+    name: req.body.name,
   };
-  genres.push(course);
-  res.send(course);
+  genres.push(genre);
+  res.send(genre);
 });
 
-//Update
 app.put("/api/genres/:id", (req, res) => {
-  const gn = genres.find((x) => {
-    return x.id == req.params.id;
-  });
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
 
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+  res.send(genre);
+});
+
+function validateGenre(genre) {
   const schema = {
-    genre: Joi.string().min(3).required(),
+    name: Joi.string().min(3).required(),
   };
 
-  const result = Joi.validate(req.body, schema);
+  return Joi.validate(genre, schema);
+}
 
-  if (result.error) {
-    //400 Bad request
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
-
-  gn.genre = req.body.genre;
-  res.send(gn);
-});
-
-//delete
-app.delete("/api/genres/:id", (req, res) => {
-  const gn = genres.find((x) => {
-    return x.id == req.params.id;
-  });
-
-  let reso = genres.indexOf(gn);
-  genres.splice(reso);
-  res.send(gn);
-});
-
-app.listen(3000, () => {
-  console.log("app is listening on port 3000");
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
